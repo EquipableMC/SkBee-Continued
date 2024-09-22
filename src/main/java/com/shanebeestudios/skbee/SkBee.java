42c059c0e365a25e8ab804b800f8fd0e0c44f68f
EquipableMC <66171067+EquipableMC@users.noreply.github.com>
2024-09-22 11:53:30 -0400

    read description
    changes bstats id from SkAssist to SkBee-Continued

--- src/main/java/com/shanebeestudios/skbee/SkBee.java
+++ src/main/java/com/shanebeestudios/skbee/SkBee.java
@@ -92,7 +92,7 @@ public class SkBee extends JavaPlugin {
     }
 
     private void loadMetrics() { //6719
-        Metrics metrics = new Metrics(this, 6719);
+        Metrics metrics = new Metrics(this, 23443);
         metrics.addCustomChart(new SimplePie("skript_version", () -> Skript.getVersion().toString()));
         metrics.addCustomChart(new SimplePie("virtual_furnace", () -> String.valueOf(config.ELEMENTS_VIRTUAL_FURNACE)));
     }
